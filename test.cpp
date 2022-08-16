9e87198e55a959d5f60210d9a2ee386ad0010374
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-08-16 23:29:03 +0000

    test

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+  int cars[10] = {4, 3, 5, 1, 6, 7, 8, 5, 4, 3};
+  for (int i = 0; i < 10; i++) { 
+    cout << cars[i] << "\n"; 
+} 
+}
