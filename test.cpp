0fce65ff3b67358ca1187cc196cd4a114e062b30
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-08-16 23:52:44 +0000

    boob

--- test.cpp
+++ test.cpp
@@ -4,15 +4,14 @@ using namespace std;
 
 
 int main() {
-  int cars[10] = {4, 3, 5, 1, 6, 7, 8, 5, 4, 3};
 
 
   int answer;
   cout << "Press 1 if gay.\n";
   cin >> answer;
 
-  if (answer == 1) cout << "oh no\n\n";
-  if (answer != 1) cout << "lesgo dababy\n\n";
+  if (answer == 1) { cout << "oh no\n\n"; }
+  if (answer != 1) cout << "kris is cool\n\n";
 
   return 0;
 }
